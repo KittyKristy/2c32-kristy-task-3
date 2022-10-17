e7e16b2b076d83f0ea973cea53e187726a60a306
KittyKristy <115450884+KittyKristy@users.noreply.github.com>
2022-10-17 14:27:41 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    count = 6
+    basic.showNumber(count)
+    for (let index = 0; index < 9; index++) {
+        count += -1
+        basic.showNumber(count)
+    }
+})
+basic.forever(function () {
+	
+})
